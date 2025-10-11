faacc7b492b001a32833a289f6029cbac1e080de
techaryahs <sah.nai.rt22@dypatil.edu>
2025-10-11 12:48:46 +0530

    Fix lazy imports in __init__.py to avoid redis dependency at import time

--- libretranslate/__init__.py
+++ libretranslate/__init__.py
@@ -1,3 +1,10 @@
 import os
-from .main import main
-from .manage import manage
+
+# Lazy imports to avoid loading all dependencies at package import time
+def main():
+    from .main import main as _main
+    return _main()
+
+def manage():
+    from .manage import manage as _manage
+    return _manage()
